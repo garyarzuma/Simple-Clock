d28cfce432612a5d1db9bc0eb833b88d8f1527e6
Gary Arzumanyan <garyarzumanyan@gmail.com>
2021-01-02 15:00:16 -0800

    initial commit

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour {
+  	const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+	[SerializeField]
+	Transform hoursPivot = default, minutesPivot = default, secondsPivot = default;
+
+	void Awake () {
+      var time = DateTime.Now;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+	}
+};
